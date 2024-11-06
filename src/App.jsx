ab08b8723d4666e0b35ec2292d71fbd5192ca705
sairatom <sairavtom16@gmail.com>
2024-11-06 14:41:13 +0530

    formik

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegistrationForm from './component/registration form';
+
+function App() {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
